d96cc331e130ede065982d8693070898a298aa4a
Computhink-CS138 <cs138@computhink.com.sg>
2026-07-11 07:26:51 +0000

    Jul 11, 2026, 7:26 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -29,7 +29,7 @@ function draw() {
 
   // Challenge 4: A rocketship using triangle() function
   triangle(250, 50, 320, 170, 180, 170)
-  rect()
+  rect(100, 200, 100, 200)
 
   // Challenge 5: A square face using quad() function
 
