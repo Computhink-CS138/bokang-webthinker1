1dbdf03ef295284a3ff76ccfc80604b138faabc7
Computhink-CS138 <cs138@computhink.com.sg>
2026-07-18 06:55:53 +0000

    Jul 18, 2026, 6:55 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -6,9 +6,9 @@ function setup() {
   
 function draw() {
   // Recap 1: Repeating Circles
-// circle(75, 200, 100);
-// circle(225, 200, 100);
-// circle(375, 200, 100);
+  // circle(75, 200, 100);
+  // circle(225, 200, 100);
+  // circle(375, 200, 100);
 // circle(540, 200, 100);
 
 const pi = 3.14159265389;
