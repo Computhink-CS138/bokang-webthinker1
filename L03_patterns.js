39784eaef06c8894147c0d5be701dc0410a1d4dc
Computhink-CS138 <cs138@computhink.com.sg>
2026-07-25 06:46:34 +0000

    Jul 25, 2026, 6:46 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -26,10 +26,10 @@ function draw() {
 
   // Task 2: Colour Loop
   for (let i = 0; i <= 14; i++) {
-  for (let i = 0; i <= 14; i++) {
-    if (i % 2 == 0){
-      fill(0);
-    } else {
+    for (let i = 0; i <= 14; i++) {
+      if (i % 2 == 0){
+        fill(0);
+      } else {
       fill(255)
     }
     rect(0 + i * 40, 0, 40, 40);
