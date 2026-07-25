2fd8894b08354f485e6ba136e04aa102d731beff
Computhink-CS138 <cs138@computhink.com.sg>
2026-07-25 07:30:40 +0000

    Jul 25, 2026, 7:30 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -40,14 +40,13 @@ function draw() {
   let numCircles = 5;
   let totalWidth = numCircles * circleDiameter;
   let startX = (width - totalWidth) / 2 + circleDiameter / 2;
-  for (grow = 0; row < 5; )
-
-  for(let i = 0; i < numCircles; i++) {
+  for (grow = 0; row < 5; row++){
+for(let i = 0; i < numCircles; i++) {
     let x = startX + i * circleDiameter; 
     ellipse(x, height / 2, circleDiameter, circleDiameter);
 
   }
-
+  }
 
   // Task 4: Grid of Circles
 }
