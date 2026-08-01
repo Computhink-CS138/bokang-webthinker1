ceda1deaac08b4cb46fef9e5db13161b0f35e507
Computhink-CS138 <cs138@computhink.com.sg>
2026-08-01 06:56:36 +0000

    Aug 1, 2026, 6:56 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -21,6 +21,7 @@
 //     fill(ranNum);
 //     ellipse(200, 200, 80, 80);
 // }
+let 
 function setup() {
     createCanvas(400, 400);
     background(220);
@@ -28,5 +29,6 @@ function setup() {
 }
 
 function mouseMoved() {
+
     ellipse(mouseX, mouseY, 10, 10);
 }
