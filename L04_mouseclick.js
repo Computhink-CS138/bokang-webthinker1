f15fdd04a908a254eb83de03fb8d29497392e8e7
Computhink-CS138 <cs138@computhink.com.sg>
2026-08-01 06:26:53 +0000

    Aug 1, 2026, 6:26 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -1,5 +1,5 @@
 let shapeColour = 'blue';
-let ranNum = colour(random(255))
+let ranNum = colour(random(255), random(255), random(255))
 function setup() {
     createCanvas(400, 400);
     background(220);
