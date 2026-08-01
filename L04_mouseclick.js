0e96d4e2711c2522387c017601d57c3add78916b
Computhink-CS138 <cs138@computhink.com.sg>
2026-08-01 06:43:13 +0000

    Aug 1, 2026, 6:43 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -1,23 +1,26 @@
-let shapeColour = 'blue';
+// let shapeColour = 'blue';
 
-function setup() {
-    createCanvas(400, 400);
-    background(220);
-}
+// function setup() {
+//     createCanvas(400, 400);
+//     background(220);
+// }
 
-function draw() {
-    fill(shapeColour);
+// function draw() {
+//     fill(shapeColour);
     
-}
+// }
 
-function mousePressed() {
-    let ranNum = color(random(255), random(255), random(255));
-    fill(ranNum);
-    ellipse(200, 200, 80, 80);
-}
+// function mousePressed() {
+//     let ranNum = color(random(255), random(255), random(255));
+//     fill(ranNum);
+//     ellipse(200, 200, 80, 80);
+// }
 
-function mouseReleased() {
-    let ranNum = color(255, 255, 255);
-    fill(ranNum);
-    ellipse(200, 200, 80, 80);
+// function mouseReleased() {
+//     let ranNum = color(255, 255, 255);
+//     fill(ranNum);
+//     ellipse(200, 200, 80, 80);
+// }
+function setup() {
+    create
 }
