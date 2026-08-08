a10cbc50fe9ba020d434851058d66bd545bb36d1
Computhink-CS138 <cs138@computhink.com.sg>
2026-08-08 07:27:13 +0000

    Aug 8, 2026, 7:27 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -28,7 +28,7 @@ function setup() {
 
 function keyPressed() {
     console.log("key:", key);
-    console
+    console.log("keycode:", )
     if (key == 'c') {
         circle(200, 200, 50);
     elif (keycode == '32')
