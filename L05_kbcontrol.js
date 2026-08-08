39057f5ccaf4a0ea83b0eda4e5a8d51509d55219
Computhink-CS138 <cs138@computhink.com.sg>
2026-08-08 06:58:00 +0000

    Aug 8, 2026, 6:58 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -10,7 +10,9 @@ function setup() {
 function keyPressed() {
     let numCircle = random(1, 20);
     for (let i = 0; i < numCircle; i++) {
-        let colour = random(0, 255);
+        let colour1 = random(0, 255);
+        let colour2 = random(0, 255);
+        let colour3 = random(0, 255);
         let rectSize = random(10, 50);
         let random1 = random(0, 400);
         let random2 = random(0, 400);
