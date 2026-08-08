8d60dd4572badfcad042bd70a17f9ed9f6549c05
Computhink-CS138 <cs138@computhink.com.sg>
2026-08-08 06:56:12 +0000

    Aug 8, 2026, 6:56 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,6 +1,6 @@
 function setup() {
     createCanvas(400, 400);
-    // background(220);
+    background(220);
 }
 
 // function draw() {
@@ -8,17 +8,16 @@ function setup() {
 // }
 
 function keyPressed() {
-    // let numCircle = random(1, 20);
-    // for (i = 0; i < numCircle, i++;) {
-    //     let colour = random(0, 255);
-    //     let rectSize = random(10, 50);
-    //     let random1 = random(0, 400);
-    //     let random2 = random(0, 400);
-    //     fill(colour, colour, colour);
-    //     ellipse(random1, random2, rectSize, rectSize);
+    let numCircle = random(1, 20);
+    for (i = 0; i < numCircle, i++;) {
+        let colour = random(0, 255);
+        let rectSize = random(10, 50);
+        let random1 = random(0, 400);
+        let random2 = random(0, 400);
+        fill(colour, colour, colour);
+        ellipse(random1, random2, rectSize, rectSize);
         
-    // }
-    background(200)
+    }
 }
 
 function keyReleased() {
