d9498ad24252e8895c643c54a106e6cccd96f7bf
Computhink-CS138 <cs138@computhink.com.sg>
2026-08-15 07:08:10 +0000

    Aug 15, 2026, 7:08 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -14,12 +14,12 @@ function draw() {
         x -= 10;
     }
     circle(width / 2 + x, height / 2, 100);
-    if (x < 25) {
-        x = 25;
-    }
-    if (x > width - 25) {
-        x = width - 25;
-    }
+    // if (x < 25) {
+    //     x = 25;
+    // }
+    // if (x > width - 25) {
+    //     x = width - 25;
+    // }
 }
 
 
