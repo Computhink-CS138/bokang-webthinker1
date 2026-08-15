39f86ce149d6144a932af3b11d76c35d94b7d425
Computhink-CS138 <cs138@computhink.com.sg>
2026-08-15 07:26:07 +0000

    Aug 15, 2026, 7:26 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -8,7 +8,7 @@ function draw() {
     if (keyIsDown(RIGHT_ARROW)) {
         x += 10;
     }
-    tiangle(x, x + 30, x + 10, x + 20, x + 40, x )
+    tiangle(x, x + 30, x + 10, x + 20, x + 40, x + 50);
 
     if (keyIsDown(LEFT_ARROW)) {
         x -= 10;
