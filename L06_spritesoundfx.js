a8f190cb3c1a99feeaa9ce7aff899c04c4228fb5
Computhink-CS138 <cs138@computhink.com.sg>
2026-08-22 06:22:18 +0000

    Aug 22, 2026, 6:22 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -30,7 +30,7 @@ function draw() {
     }
 
     y = constrain(y, 0, height);
-    x = constrain(x, 0 , height);
+    x = constrain(x, 0, height);
 
     if (direction == "up") {
         triangle(75, y + 25, 100, y - 25, 125, y + 25);
