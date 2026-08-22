094b33303dcfbd99076a2cf5503b5795daa98ddb
Computhink-CS138 <cs138@computhink.com.sg>
2026-08-22 06:47:39 +0000

    Aug 22, 2026, 6:47 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,52 +1,52 @@
-let x = 100
-let y = 100
-let direction = "up"
+// let x = 100
+// let y = 100
+// let direction = "up"
 
-function setup() {
-    createCanvas(400, 400);
-    background(220);
-}
+// function setup() {
+//     createCanvas(400, 400);
+//     background(220);
+// }
 
-function draw() {
-    background(220);
-    if (keyIsDown(UP_ARROW)) {
-        y -= 5;
-        direction = "up";
-    }
+// function draw() {
+//     background(220);
+//     if (keyIsDown(UP_ARROW)) {
+//         y -= 5;
+//         direction = "up";
+//     }
 
-    if (keyIsDown(DOWN_ARROW)) {
-        y += 5;
-        direction = "down";
-    }
+//     if (keyIsDown(DOWN_ARROW)) {
+//         y += 5;
+//         direction = "down";
+//     }
 
-    if (keyIsDown(LEFT_ARROW)) {
-        x -= 5;
-        direction = "left";
-    }
+//     if (keyIsDown(LEFT_ARROW)) {
+//         x -= 5;
+//         direction = "left";
+//     }
 
-    if (keyIsDown(RIGHT_ARROW)) {
-        x += 5;
-        direction = "right";
-    }
+//     if (keyIsDown(RIGHT_ARROW)) {
+//         x += 5;
+//         direction = "right";
+//     }
 
-    y = constrain(y, 50, 350 - 50);
-    x = constrain(x, 50, 350 - 50);
+//     y = constrain(y, 50, 350 - 50);
+//     x = constrain(x, 50, 350 - 50);
 
-    if (keyIsDown(32)) {
-        fill(200)
-    } else {
-        fill(50)
-    }
+//     if (keyIsDown(32)) {
+//         fill(200)
+//     } else {
+//         fill(50)
+//     }
 
-    if (direction == "up") {
-        rect(x, y, 50, 50);
-    } else if (direction == "down") {
-        rect(x, y, 50, 50);
-    } else if (direction == "left") {
-        rect(x, y, 50, 50);
-    } else if (direction == "right") {
-        rect(x, y, 50, 50);
-    }
-}
+//     if (direction == "up") {
+//         rect(x, y, 50, 50);
+//     } else if (direction == "down") {
+//         rect(x, y, 50, 50);
+//     } else if (direction == "left") {
+//         rect(x, y, 50, 50);
+//     } else if (direction == "right") {
+//         rect(x, y, 50, 50);
+//     }
+// }
 
 
