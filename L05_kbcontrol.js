f449ab2a1692b404d4a5f74f1345b90fb184d211
Computhink-CS138 <cs138@computhink.com.sg>
2026-08-22 06:28:26 +0000

    Aug 22, 2026, 6:28 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,31 +1,31 @@
-function setup() {
-    createCanvas(400, 400);
-    background(220);
-}
-let x = 0
-let y = 100
-let direction = "up"
+// function setup() {
+//     createCanvas(400, 400);
+//     background(220);
+// }
+// let x = 0
+// let y = 100
+// let direction = "up"
 
-function draw() {
-    background(220);
-    if (keyIsDown(UP_ARROW)) {
-        y -= 2;
-        direction = "up";
-    }
+// function draw() {
+//     background(220);
+//     if (keyIsDown(UP_ARROW)) {
+//         y -= 2;
+//         direction = "up";
+//     }
 
-    if (keyIsDown(DOWN_ARROW)) {
-        y += 2;
-        direction = "down";
-    }
+//     if (keyIsDown(DOWN_ARROW)) {
+//         y += 2;
+//         direction = "down";
+//     }
 
-    let y = constrain(y, 0, height);
+//     let y = constrain(y, 0, height);
 
-    if (direction == "up") {
-        triangle(75, y + 25, 100, y - 25, 125, y + 25);
-    } else if (direction == "down") {
-        triangle(75, y - 25, 100, y + 25, 125, y - 25);
-    }
-}
+//     if (direction == "up") {
+//         triangle(75, y + 25, 100, y - 25, 125, y + 25);
+//     } else if (direction == "down") {
+//         triangle(75, y - 25, 100, y + 25, 125, y - 25);
+//     }
+// }
 
 
 
