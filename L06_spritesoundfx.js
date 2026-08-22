9325575339b55fa627a2384e3cc697adb15ea72e
Computhink-CS138 <cs138@computhink.com.sg>
2026-08-22 07:30:22 +0000

    Aug 22, 2026, 7:30 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -86,7 +86,7 @@ function draw() {
     y = constrain(y, 50, width - 100);
     x = constrain(x, 50, height - 100);
 
-    if (x === 400 || x === 0 || y === 400 || y === 0) {
+    if (x === width - 100 || x === 0 || y === 400 || y === 0) {
     background("red");
     popSound.play();
     } 
