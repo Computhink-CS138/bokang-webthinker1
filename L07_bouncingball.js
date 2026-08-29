e34f8e913d6d83b0f7f66ccf82d26876d7b652c6
Computhink-CS138 <cs138@computhink.com.sg>
2026-08-29 06:25:58 +0000

    Aug 29, 2026, 6:25 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -34,6 +34,7 @@ function draw() {
 
     y = constrain(y, 0, width - 100);
     x = constrain(x, 0, height - 100);
+    
     image(picoImage, x, y, 100, 100);
      // (image, x, y, width, height);
 
