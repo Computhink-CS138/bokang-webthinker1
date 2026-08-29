40fba7c59699c40ccbffd1c0edd0692778783b8b
Computhink-CS138 <cs138@computhink.com.sg>
2026-08-29 06:11:04 +0000

    Aug 29, 2026, 6:11 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -108,8 +108,12 @@
 
 let picoImage;
 let popSound; 
+
 function preload() {
     picoImage = loadImage('assets/pico-a.png');
     popSound = loadSound('assets/pop.mp3');
 }
 
+function setup() {
+    
+}
