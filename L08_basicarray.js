03b9e06ac03d98f8bf1cc9f3f490669a66cd92dd
Computhink-CS138 <cs138@computhink.com.sg>
2026-09-12 06:54:13 +0000

    Sep 12, 2026, 6:54 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -6,9 +6,12 @@ function setup() {
     createCanvas(400, 400);
     background(220);
 
-    textSize(40);
+    textSize(10);
     textAlign(CENTER, CENTER);
     text("Hello!", width / 2, height / 2); // (string, x pos, y pos);
+    text("My name is bokang", 50, 50);
+    text("My age is 13", 50, 100);
+    text("I like playing video games", 50, 150);
 }
 
 function text() {
