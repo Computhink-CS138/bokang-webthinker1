67e0cbf0afb54d554449e82192847ac9a68aca93
Computhink-CS138 <cs138@computhink.com.sg>
2026-09-19 06:38:49 +0000

    Sep 19, 2026, 6:38 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -30,4 +30,6 @@ function draw() {
 
     text("asd", width / 2, yPos);
     yPos -= 1;
+    for (let i = 0; i < favouriteFoods.length; i++) {
+        text((i + 1) + ". " + favouriteFoods[i], 50, Ypos + i * 30);
 }
