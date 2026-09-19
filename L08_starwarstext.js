4588451dab44d6736803886fbc61f29725778ce3
Computhink-CS138 <cs138@computhink.com.sg>
2026-09-19 06:48:32 +0000

    Sep 19, 2026, 6:48 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -30,7 +30,7 @@ function draw() {
 
   
     for (let i = 0; i < storyText.length; i++) {
-        text(storyText[i], width / 2, yPos + 1 * 50);
+        text(storyText[i], width / 2, yPos + i * 50);
 
     
     yPos -= 1;
