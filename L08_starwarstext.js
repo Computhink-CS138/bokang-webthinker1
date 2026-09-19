abe65d233198539c0020df4be97c5a172444f740
Computhink-CS138 <cs138@computhink.com.sg>
2026-09-19 06:44:53 +0000

    Sep 19, 2026, 6:44 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -18,7 +18,7 @@ let storyText = [
 function setup() {
     createCanvas(600, 400);
     textSize(24);
-    textAlign(LEFT, CENTER);
+    textAlign(CENTER, CENTER);
     fill("rgb(255, 255, 0)");
 
     yPos = height;
